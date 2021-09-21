003327e8ecac87ed744333421b362e6c7e73dd49
Wuttiong Wongthip <61160234@go.buu.ac.th>
2021-09-21 20:17:12 +0700

    Test Exercise

--- index.js
+++ index.js
@@ -14,18 +14,3 @@ app.listen(port, () => {
     console.log('The app is up and running!')
 })
 module.exports = app
-
-const request = require('supertest')
-const app = require('../index')
-
-describe('GET /', () => {
-    it('responds with "Hello world!!!"', (done) => {
-        request(app).get('/').expect('Hello world!!!', done)
-    })
-})
-
-describe('GET /users', () => {
-    it('responds with "No user in the system"', (done) => {
-        request(app).get('/users').expect('No user in the system', done)
-    })
-})
